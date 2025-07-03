376f95f84a271f3737d813188bf949cc42906b27
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-03 17:31:51 +0700

    add home link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -30,13 +30,12 @@
 </script>
 
 <footer>
-	<div class="content">
-		<p class="footer-rw">
-			The content on the page is intended for Review, which means that the information provided on
-			the page is intended for users to view and study. It does not require any active actions from
-			the user, except for reading or viewing.
-		</p>
-
+	<div class="content content-0">
+		<div>
+			<a href={base}>Home</a>
+		</div>
+	</div>
+	<div class="content content-1">
 		<div class="footer-links">
 			{#each zodiacSigns as sign}
 				<a href="{base}/{sign}/" data-sign={sign} onclick={setSignStore}
@@ -44,10 +43,20 @@
 				>
 			{/each}
 		</div>
+		<p class="footer-rw">
+			The content on the page is intended for Review, which means that the information provided on
+			the page is intended for users to view and study. It does not require any active actions from
+			the user, except for reading or viewing.
+		</p>
 	</div>
 </footer>
 
 <style>
+	.content-0 div {
+		border-bottom: 1px solid rgba(177, 134, 231, 0.5);
+		padding-bottom: 12px;
+		margin-bottom: 12px;
+	}
 	.footer-rw {
 		color: hsl(261, 66%, 82%);
 	}
@@ -77,7 +86,7 @@
 		gap: 8px;
 	}
 
-	.content {
+	.content-1 {
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
 		gap: 48px;
@@ -96,7 +105,7 @@
 	}
 
 	@media screen and (max-width: 550px) {
-		.content {
+		.content-1 {
 			grid-template-columns: repeat(1, 1fr);
 		}
 	}
